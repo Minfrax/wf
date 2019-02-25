9ccc495c3cae1448feecce52b5807ede3c454147
Jerry <hoffmann.jerry88@gmail.com>
2019-02-25 12:18:00 +0100

    Finished second exercise

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,22 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+$test = strlen($password) / 2;
+// echo ($test);
+
+$newPswd = substr($password, 0, $test) .$salt .substr($password, -$test);
+
+// Second way, with ceil
+
+$pos = ceil(strlen($password) / 2);
+$secondPswd = substr($password, 0, $pos) . $salt . substr($password, $pos);
+
+// Bonus part with ceil and floor
+
+$firstPart = substr($password, 0, floor(strlen($password) / 2) + (strlen($password) % 2));
+$lastPart = substr($password, ceil(strlen($password) / 2));
+$finalPass = $firstPart . $salt . $lastPart;
+
+echo ($finalPass);
+
+$saltedPassword = $finalPass;
