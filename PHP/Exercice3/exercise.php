f41fe4a7f7ab64b5d9a290c554afbfe5dcc7c454
Jerry <hoffmann.jerry88@gmail.com>
2019-02-25 16:46:35 +0100

    Finished third exercise

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -2,3 +2,59 @@
 
 $input;
 $winner;
+//var_dump($input);
+
+$index = 0;
+$indexB = 0;
+
+
+foreach ($input as $row) {
+
+    if($row[0] > $row[1]) {
+        $index++;
+        echo($index);
+    } else if ($row[0] < $row[1]) {
+        $indexB++;
+        echo($indexB);
+    }
+}
+
+if ($index > $indexB) {
+    $winner = "A";
+} else {
+    $winner = "B";
+}
+
+echo('The winner is '. $winner);
+
+
+// SECOND WAY -> With functions
+
+/**
+ * Get winner
+ *
+ * Return the winner of a game containing multiple rounds
+ *
+ * @param array[] $input The input containing rounds
+ *
+ * @return string
+ */
+function getWinner(array $input) : string
+{
+    $brotherA = 0;
+    $brotherB = 0;
+
+    foreach ($input as [$cardA, $cardB]) {
+        if ($cardA > $cardB) {
+            $brotherA++;
+        } else if ($cardA < $cardB) {
+            $brotherB++;
+        }
+    }
+    if ($brotherA > $brotherB) {
+        return 'A';
+    }
+    return 'B';
+}
+
+$winner = getWinner($input);
