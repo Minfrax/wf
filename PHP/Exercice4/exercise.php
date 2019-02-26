c9d0dd0f2bf12ec64f63e5e441dae276d71e2f7c
Jerry <hoffmann.jerry88@gmail.com>
2019-02-26 15:03:19 +0100

    Finished 4th exercise

--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,1 +1,27 @@
 <?php
+//$interval = DateInterval::createFromDateString('+1day');
+// var_dump($date);
+
+$test = [];
+
+// function to get every monday of the month -> given the parameters year and month
+function getAllMondaysOfMonth(int $year, int $month) : array {
+    $allMondays = [];
+    // -> new DateTime -> "first monday of" is a relative time format -> Look at php manual || Y -> full numeric rep. of the year || n -> numeric rep. of the month
+    //$secDate = new DateTime('first monday'.(DateTime::createFromFormat('Yn', $year.$month))->format('M Y'));
+    $myDate = DateTime::createFromFormat('Yn', $year.$month);
+    $myDate = new DateTime('first monday of '.$myDate->format('F Y'));
+
+    // -> same for "next monday" this is also a relative time format
+    $interval = DateInterval::createFromDateString('next monday of');
+    while ($myDate -> format("m") === $month ) {
+        // l in the format stands for the textual representation of the day
+        $allMondays = $myDate->format('l j, M Y');
+        //three manipulate methods for dates -> add, sub & diff
+        $myDate->add($interval);
+        //var_dump($allMondays);
+    }
+    //var_dump($secDate);
+    return $allMondays;
+}
+
