9d3a94a70c12d781a943f8fdcc4352fa00bbcb29
Jerry <hoffmann.jerry88@gmail.com>
2019-03-01 14:38:43 +0100

    Finished 7th exercise

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,37 @@
 <?php
+
+// throw new RuntimeException('Text');
+
+function divide($numb, $divisor) {
+
+    if($divisor == 0) {
+        throw new RuntimeException("Division by 0 is not allowed");
+    } else {
+        $res = $numb / $divisor;
+    }
+    return $res;
+}
+
+function arrayDivide(array $firstVal, int $divisor ) : array {
+    $res = [];
+
+    foreach ($firstVal as $val) {
+        try {
+            $res[] = divide($firstVal, $divisor);
+
+        } catch (RuntimeException $e) {
+            echo $e->getMessage();
+            $res[] = $val;
+        }
+    }
+    return $res;
+}
+
+/*
+    if($divisor == 0) {
+
+
+    } else {
+        return ($firstVal / $divisor);
+    }
+*/
